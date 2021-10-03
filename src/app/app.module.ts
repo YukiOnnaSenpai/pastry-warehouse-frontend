831a16cb4588f42ac6aef330db909783eb4acada
Sofija <sofi.M95@gmail.com>
2021-10-03 17:24:13 +0200

    added layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ProductEmployeeService } from './services/product-employee/product-employee.service';
+import { SupplyService } from './services/supply/supply.service';
+import { WorkOrderService } from './services/work-order/work-order.service';
 
 const Routes = [
   {path: 'employee', component: EmployeeComponent},
@@ -37,9 +52,26 @@ const Routes = [
     BrowserAnimationsModule,
     RouterModule.forRoot(Routes),
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule, 
+    MatIconModule, 
+    MatSidenavModule, 
+    MatListModule,
+    MatGridListModule, 
+    MatExpansionModule, 
+    MatSortModule, 
+    MatTableModule,
+    MatToolbarModule, 
+    // MatSelectModule, 
+    // MatOptionModule,
+    // MatSnackBarModule, 
+    MatDialogModule, 
+    MatInputModule,
+    // MatCheckboxModule, 
+    MatPaginatorModule, 
+    MatSortModule
   ],
-  providers: [],
+  providers: [ProductEmployeeService, SupplyService, WorkOrderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
